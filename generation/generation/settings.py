2465fd7e98da3c74138b974805bf208bb2de84ea
JMarts32 <j.santosa@uniandes.edu.co>
2023-05-08 08:43:25 -0500

    More dependencies and apps created

--- generation/generation/settings.py
+++ generation/generation/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "generation.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "content.sqlite3",
     }
 }
 
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es-co"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Bogota"
 
 USE_I18N = True
 
