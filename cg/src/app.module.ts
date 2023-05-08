269d01550be5a8ffbd3b0a75ba487eb21e977608
JMarts32 <j.santosa@uniandes.edu.co>
2023-05-08 11:12:16 -0500

    Dependencies installed and starting persistence

--- cg/src/app.module.ts
+++ cg/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CourseModule } from './course/course.module';
+import { ExamModule } from './exam/exam.module';
+import { QuestionModule } from './question/question.module';
+import { AnswerModule } from './answer/answer.module';
 
 @Module({
-  imports: [],
+  imports: [CourseModule, ExamModule, QuestionModule, AnswerModule],
   controllers: [AppController],
   providers: [AppService],
 })
